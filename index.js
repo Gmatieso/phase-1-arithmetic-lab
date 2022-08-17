562b0e661d2fe5f553c8bd939c001c6310555f9a
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-08-17 23:53:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+let num1 = 31,
+    num2  = 2,
+    num3 = 31,
+    num4 = 9;
+
+
+
+let  multiply = num1 * num2;
+
+
+var random = Math.floor(Math.random()* 10  ) + 1;
+
+var mod = num3 % num4;
+
+var  max = Math.max(3,5,6,7,2,20);
+
